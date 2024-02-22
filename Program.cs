f10ad298d9a3f67b0c5fa818ecab3293fffcd690
pkimmel <pkimmel@softconcepts.com>
2024-02-22 17:47:34 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+//Homework:
+//1. Test Cases
+
+//example: 5 and jeff palermo
+//example: 3 and paul (tuple) x some number of these
+//put on git hub and send link to Rayne
+
+
+using CountNumbers;
+using System.Diagnostics;
+using System.Linq.Expressions;
+
+var conditions = new List<Tuple<long, string>>() { Tuple.Create(3L, "Paul"), Tuple.Create(5L, "Kimmel") };
+
+try
+{
+  var list = new UnboundedCounter(new Rules(conditions)).GetOutput(15);
+
+  foreach (var s in list)
+  {
+    Console.WriteLine(s);
+  }
+}
+catch(Exception ex)
+{
+  Console.WriteLine(ex.Message);
+}
+
+Console.ReadLine();
+
---
+++ Sampe1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+  public class Sample1
+  {
+    public static void Count()
+    {
+      for (var i = 1; i <= 100; i++)
+      {
+
+        if (i % 3 == 0 && i % 5 == 0)
+        {
+          Console.WriteLine("Paul Kimmel");
+        }
+        else if (i % 3 == 0)
+        {
+          Console.WriteLine("Paul");
+        }
+        else if (i % 5 == 0)
+        {
+          Console.WriteLine("Kimmel");
+        }
+        else
+        {
+          Console.WriteLine(i);
+        }
+
+      }
+
+      Console.ReadLine();
+    }
+  }
+}
